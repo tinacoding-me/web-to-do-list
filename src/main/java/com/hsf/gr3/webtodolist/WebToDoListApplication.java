719b997fa723e08aa60ba91d180a7802f7d300ff
tinacoding-me <tiennhat110505@gmail.com>
2025-10-25 10:47:26 +0700

    Initial commit - new Spring Boot project

---
+++ src/main/java/com/hsf/gr3/webtodolist/WebToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.hsf.gr3.webtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebToDoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebToDoListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hsf/gr3/webtodolist/WebToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsf.gr3.webtodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebToDoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
